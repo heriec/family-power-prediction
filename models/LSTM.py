8302ab473d4cd765926993d382e324ed7889d0b3
cwhe <cwhe@stu.suda.edu.cn>
2025-06-18 22:01:23 +0800

    add: lstm

--- models/LSTM.py
+++ models/LSTM.py
@@ -4,11 +4,14 @@ import torch.nn as nn
 class LSTMModel(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size):
         super(LSTMModel, self).__init__()
+        
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
                             num_layers=num_layers, batch_first=True)
+        self.dropout = nn.Dropout(p=0.2)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         lstm_out, _ = self.lstm(x)
-        out = self.fc(lstm_out[:, -1, :])
+        lstm_out = self.dropout(lstm_out)
+        out = self.fc(lstm_out)
         return out
