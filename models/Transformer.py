5efbf2a77de140d136ddb4c0831a7573440abd11
cwhe <cwhe@stu.suda.edu.cn>
2025-06-19 18:10:10 +0800

    fix: batch_first=True

--- models/Transformer.py
+++ models/Transformer.py
@@ -6,12 +6,12 @@ class TransformerModel(nn.Module):
         super(TransformerModel, self).__init__()
         self.input_fc = nn.Linear(input_size, d_model)
         self.transformer_encoder = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(d_model, nhead), num_layers
+            nn.TransformerEncoderLayer(d_model, nhead, batch_first=True), num_layers
         )
         self.fc = nn.Linear(d_model, output_size)
 
     def forward(self, x):
         x = self.input_fc(x)
         x = self.transformer_encoder(x)
-        out = self.fc(x[:, -1, :])
+        out = self.fc(x)
         return out
